9046655c01a729348713dc53654fdf827c5b5ade
FBRAA <thefbraa@yandex.ru>
2022-01-09 02:36:55 +0500

    syntax update

--- src/components/WeatherIcon.jsx
+++ src/components/WeatherIcon.jsx
@@ -5,7 +5,7 @@ import clouds from '../images/iconsSVG/clouds.svg'
 // import fewClouds from '../images/iconsSVG/fewClouds.svg'
 import mist from '../images/iconsSVG/mist.svg'
 import rain from '../images/iconsSVG/rain.svg'
-import showerRain from '../images/iconsSVG/showerRain.svg'
+// import showerRain from '../images/iconsSVG/showerRain.svg'
 import snow from '../images/iconsSVG/snow.svg'
 import thunderstorm from '../images/iconsSVG/thunderstorm.svg'
 
@@ -19,21 +19,17 @@ const ImageTag = styled.img`
   `
 
 const WeatherIcon = ({ mainWeather }) => {
-  const allTypes = [
-    { name: 'Thunderstorm', src: thunderstorm },
-    { name: 'Drizzle', src: rain },
-    { name: 'Rain', src: showerRain },
-    { name: 'Snow', src: snow },
-    { name: 'Clear', src: clearSky },
-    { name: 'Clouds', src: clouds },
-    { name: null, src: mist },
-  ]
-  let choosePic = allTypes.find((type) => type.name === mainWeather)
-  if (choosePic === undefined) {
-    choosePic = allTypes.find((type) => type.name === null)
+  const allTypes = {
+    Thunderstorm: thunderstorm,
+    Drizzle: rain,
+    Snow: snow,
+    Clear: clearSky,
+    Clouds: clouds,
+    [undefined]: mist,
   }
+  const findPic = allTypes[mainWeather]
   return (
-    <ImageTag src={choosePic.src} alt="lule" />
+    <ImageTag src={findPic} alt="weather icon" />
   )
 }
 
